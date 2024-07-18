de1d634c95300ec554cc6d02fceb247009147aac
yhames <wjddn494@naver.com>
2024-07-18 17:41:39 +0900

    add todo

--- srcs/cylinder.c
+++ srcs/cylinder.c
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vec3.c                                             :+:      :+:    :+:   */
+/*   cylinder.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/26 18:18:53 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/07/18 17:41:14 by jeongwpa         ###   ########.fr       */
+/*   Created: 2024/07/18 17:40:47 by jeongwpa          #+#    #+#             */
+/*   Updated: 2024/07/18 17:40:54 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "vec3.h"
-#include <math.h>
+#include "cylinder.h"
 
-t_vec3	vec3(float x, float y, float z)
-{
-	t_vec3	vec;
-
-	vec.x = x;
-	vec.y = y;
-	vec.z = z;
-	return (vec);
-}
-
-float	vec_length(t_vec3 vec)
-{
-	return ((float) sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z));
-}
+// TODO
--- srcs/plane.c
+++ srcs/plane.c
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vec3.c                                             :+:      :+:    :+:   */
+/*   plane.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/26 18:18:53 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/07/18 17:41:14 by jeongwpa         ###   ########.fr       */
+/*   Created: 2024/07/18 17:38:08 by jeongwpa          #+#    #+#             */
+/*   Updated: 2024/07/18 17:38:44 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "vec3.h"
-#include <math.h>
+#include "plane.h"
 
-t_vec3	vec3(float x, float y, float z)
-{
-	t_vec3	vec;
-
-	vec.x = x;
-	vec.y = y;
-	vec.z = z;
-	return (vec);
-}
-
-float	vec_length(t_vec3 vec)
-{
-	return ((float) sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z));
-}
+// TODO

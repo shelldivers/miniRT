d176ec1ec9c6b072f2fa91ce515596b5ea0c50de
yhames <wjddn494@naver.com>
2024-07-18 17:42:38 +0900

    add todo

--- srcs/ray_utils.c
+++ srcs/ray_utils.c
@@ -6,14 +6,15 @@
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 17:13:09 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/07/18 17:41:09 by jeongwpa         ###   ########.fr       */
+/*   Updated: 2024/07/18 17:42:21 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ray.h"
 #include "vec3.h"
 
-t_vec3		point3_to_vec3(t_point3 point) {
+t_vec3		point3_to_vec3(t_point3 point)
+{
     t_vec3  vec;
 
     vec.x = point.x;
@@ -22,7 +23,8 @@ t_vec3		point3_to_vec3(t_point3 point) {
     return (vec);
 }
 
-t_point3	vec3_to_point3(t_vec3 vec) {
+t_point3	vec3_to_point3(t_vec3 vec)
+{
     t_point3    point;
 
     point.x = vec.x;

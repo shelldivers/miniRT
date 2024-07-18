3f2980b3cc6c3f7ca5f9529df573071c7404d44c
yhames <wjddn494@naver.com>
2024-07-18 17:43:03 +0900

    add todo

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:02:30 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/06/26 17:44:37 by jeongwpa         ###   ########.fr       */
+/*   Updated: 2024/07/18 17:43:00 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 extern "C" {
 # endif
 
-// todo
 int	rt_main(void);
 
 # ifdef __cplusplus

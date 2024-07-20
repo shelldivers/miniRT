671fd1f8273b906a1afa271bbe8caa030b67d28e
yhames <wjddn494@naver.com>
2024-07-21 00:38:13 +0900

    add test

---
+++ test/color/color_test.cpp
@@ -0,0 +1,45 @@
+#include "gtest/gtest.h"
+#include "color.h"
+
+TEST(ColorTest, ColorTest_color)
+{
+    t_color color;
+
+    color.r = 1;
+    color.g = 2;
+    color.b = 3;
+
+    EXPECT_EQ(color.r, 1);
+    EXPECT_EQ(color.g, 2);
+    EXPECT_EQ(color.b, 3);
+}
+
+TEST(ColorTest, ColorTest_color_to_vec3)
+{
+    t_color color;
+
+    color.r = 1;
+    color.g = 2;
+    color.b = 3;
+
+    t_vec3 vec = color_to_vec3(color);
+
+    EXPECT_EQ(vec.x, 1);
+    EXPECT_EQ(vec.y, 2);
+    EXPECT_EQ(vec.z, 3);
+}
+
+TEST(ColorTest, ColorTest_vec3_to_color)
+{
+    t_vec3 vec;
+
+    vec.x = 1;
+    vec.y = 2;
+    vec.z = 3;
+
+    t_color color = vec3_to_color(vec);
+
+    EXPECT_EQ(color.r, 1);
+    EXPECT_EQ(color.g, 2);
+    EXPECT_EQ(color.b, 3);
+}

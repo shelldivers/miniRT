c166e0c888c7c3fd84a305f93f4ffe08ede2edc8
yhames <wjddn494@naver.com>
2024-07-20 22:34:08 +0900

    fix CMakeLists

--- minirt.c
+++ minirt.c
@@ -6,23 +6,15 @@
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:02:33 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/06/26 17:46:14 by jeongwpa         ###   ########.fr       */
+/*   Updated: 2024/07/20 22:30:44 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "ft_printf.h"
 
-int	rt_main(void)
-{
-	ft_printf("Hello, miniRT!\n");
-	return (0);
-}
-
-/*
 int	main(void)
 {
 	ft_printf("Hello, miniRT!\n");
 	return (0);
 }
-*/

16cdef88e8de8eaba38f84f5dc851facff7c3356
yhames <wjddn494@naver.com>
2024-07-21 00:39:07 +0900

    add test

--- test/vec3/vec3_test.cpp
+++ test/vec3/vec3_test.cpp
@@ -59,3 +59,24 @@ TEST(Vec3Test, Vec3Test_vec_length)
 
 	EXPECT_FLOAT_EQ(length, 3.7416573867739413);
 }
+
+TEST(Vec3Test, Vec3Test_vec3_unit)
+{
+	t_vec3	vec = vec3(1, 2, 3);
+	t_vec3	unit = vec3_unit(vec);
+
+	EXPECT_FLOAT_EQ(unit.x, 0.267261236906051);
+	EXPECT_FLOAT_EQ(unit.y, 0.534522483812051);
+	EXPECT_FLOAT_EQ(unit.z, 0.801783729618999);
+}
+
+TEST(Vec3Test, Vec3Test_vec3_div)
+{
+	t_vec3	vec = vec3(1, 2, 3);
+	float	t = 2;
+	t_vec3	div = vec3_div(vec, t);
+
+	EXPECT_FLOAT_EQ(div.x, 0.5);
+	EXPECT_FLOAT_EQ(div.y, 1);
+	EXPECT_FLOAT_EQ(div.z, 1.5);
+}

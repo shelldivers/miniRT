0ed887db6a18ec272d525e9858e237c2fca4c4ab
yhames <wjddn494@naver.com>
2024-07-23 00:29:12 +0900

    chore: add mlx

--- minirt.c
+++ minirt.c
@@ -6,12 +6,13 @@
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:02:33 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/07/22 22:53:41 by jeongwpa         ###   ########.fr       */
+/*   Updated: 2024/07/23 00:28:19 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "ft_printf.h"
+#include "mlx.h"
 
 int	main(void)
 {

a58c897575f8a3be31e7291ecfd8af0fea1bc250
yhames <wjddn494@naver.com>
2024-07-25 22:54:30 +0900

    fix: change order win init

--- minirt.c
+++ minirt.c
@@ -6,7 +6,7 @@
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:02:33 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/07/25 22:49:36 by jeongwpa         ###   ########.fr       */
+/*   Updated: 2024/07/25 22:51:58 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,14 @@ int	main(void)
 	if (!mlx)
 		error_exit("mlx_init() failed");
 	init_image(&img, mlx);
+	win = mlx_new_window(mlx, img.width, img.height, "miniRT");
+	if (!win)
+		error_exit("mlx_new_window() failed");
 	camera.view_point = (t_vec3){0, 0, 0};
 	camera.normal = (t_vec3){0, 0, 0};
 	camera.fov = 70;
 	init_viewport(&img, &camera, &viewport);
 	ray_tracing(&img, &camera, &viewport);
-	win = mlx_new_window(mlx, img.width, img.height, "miniRT");
-	if (!win)
-		error_exit("mlx_new_window() failed");
 	mlx_put_image_to_window(mlx, win, img.ptr, 0, 0);
 	mlx_loop(mlx);
 	return (EXIT_SUCCESS);

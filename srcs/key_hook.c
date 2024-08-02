afaeaba83f5f9accfa8a998672cd4e1016628017
yhames <wjddn494@naver.com>
2024-08-03 00:03:19 +0900

    feat: add basic functions in key_hook

--- srcs/key_hook.c
+++ srcs/key_hook.c
@@ -3,12 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   key_hook.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeongwpa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 17:55:15 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/08/01 17:55:16 by jeongwpa         ###   ########.fr       */
+/*   Updated: 2024/08/03 00:02:29 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "mlx.h"
+#include <stdlib.h>
+
+int	key_press(int keycode, t_rt *rt)
+{
+	if (keycode == KEY_ESC)
+		destroy_minirt(rt);
+	return (0);
+}
+
+int	destroy_minirt(t_rt *rt)
+{
+	mlx_destroy_window(rt->mlx, rt->win);
+	exit(EXIT_SUCCESS);
+}

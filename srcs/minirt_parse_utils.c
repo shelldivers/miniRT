bfca53cb74b639b1ea578d43bf1964418372fe4a
yhames <wjddn494@naver.com>
2024-08-07 20:02:21 +0900

    feat: add parse_vec3

--- srcs/minirt_parse_utils.c
+++ srcs/minirt_parse_utils.c
@@ -11,17 +11,28 @@
 /* ************************************************************************** */
 
 #include "minirt.h"
+#include "libft.h"
 
-t_vec3	parse_vec3(char *line)
+/**
+ * @brief parse vec3 from line with format "1.0,2.0,3.0"
+ * @remark it is assumed that the line is in the correct format
+ * @param line		"1.0,2.0,3.0"
+ * @return t_vec3
+*/
+t_vec3	parse_vec3(char const *line)
 {
 	t_vec3	vec;
 
 	vec.x = ft_atof(line);
-	while (*line && !ft_isspace(*line))
-		++line;
+	while (*line && *line != ',')
+		line++;
+	if (*line && *line == ',')
+		line++;
 	vec.y = ft_atof(line);
-	while (*line && !ft_isspace(*line))
-		++line;
+	while (*line && *line != ',')
+		line++;
+	if (*line && *line == ',')
+		line++;
 	vec.z = ft_atof(line);
 	return (vec);
 }

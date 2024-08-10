11f8f050907e744b8fba2988500218b2a40182a4
yhames <wjddn494@naver.com>
2024-08-10 23:04:04 +0900

    test: add testcase

--- test/parse_rtfile_test.cpp
+++ test/parse_rtfile_test.cpp
@@ -301,3 +301,27 @@ TEST(parse_rtfile_test, 식별자_앞에_들여쓰기가_있는_경우)
 	remove(filename.c_str());
 }
 
+TEST(parse_rtfile_test, 허용되지_않은_식별자인_경우)
+{
+    std::string filename = "example.rt";
+    std::ofstream file(filename, std::ios::out | std::ios::trunc);
+    if (file.is_open()) {
+        file << "A 0.1 255,255,255\n";
+        file << "C 0,0,0 0,0,1 70\n";
+        file << "cy 0,0,0 0,0,1 1 1 255,255,255\n";
+        file << "sp 0,0,0 1 255,255,255\n";
+        file << "Ab 0,0,0 0,0,1 255,255,255";
+        file.close();
+    } else {
+        std::cout << "Unable to open test file" << std::endl;
+    }
+
+	int fd = open(filename.c_str(), O_RDONLY);
+	t_cam *cam = (t_cam *)malloc(sizeof(t_cam));
+	t_hit_lst *world = init_hittable_list(10);
+
+	ASSERT_THROW(parse_rtfile(fd, cam, world), std::runtime_error);
+
+	close(fd);
+	remove(filename.c_str());
+}

77c26a84768a6cadbc19944c9c199dd4a584e790
yhames <wjddn494@naver.com>
2024-08-18 22:08:26 +0900

    feat: add fov of camera

--- srcs/minirt_utils.c
+++ srcs/minirt_utils.c
@@ -6,13 +6,16 @@
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 00:16:07 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/08/03 00:42:53 by jeongwpa         ###   ########.fr       */
+/*   Updated: 2024/08/18 22:07:40 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "error.h"
 #include <mlx.h>
+#include <math.h>
+
+static float	degree_to_radian(float degree);
 
 void	init_mlx(t_rt *rt, t_img *img)
 {
@@ -46,7 +49,7 @@ void	init_viewport(t_img *img, t_camera *camera, t_viewport *viewport)
 	t_vec3		viewport_v;
 
 	viewport->focal_length = 1.0;
-	viewport_height = 2.0;
+	viewport_height = 2.0 * tan(degree_to_radian(camera->fov) / 2.0);
 	viewport_width = viewport_height * ((float)img->width / img->height);
 	viewport_u = (t_vec3){viewport_width, 0, 0};
 	viewport_v = (t_vec3){0, -viewport_height, 0};
@@ -62,4 +65,9 @@ void	init_viewport(t_img *img, t_camera *camera, t_viewport *viewport)
 		vec3_mul(viewport->pixel_delta_u, 0.5));
 	viewport->pixel00_loc = vec3_add(viewport->pixel00_loc, \
 		vec3_mul(viewport->pixel_delta_v, 0.5));
+}
+
+float	degree_to_radian(float degree)
+{
+	return (degree * M_PI / 180.0);
 }

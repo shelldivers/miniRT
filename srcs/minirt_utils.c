65513c4813a2c58ce1ac9076c162484158a145af
yhames <wjddn494@naver.com>
2024-08-18 22:15:42 +0900

    feat: add normal vector in camera

--- srcs/minirt_utils.c
+++ srcs/minirt_utils.c
@@ -6,7 +6,7 @@
 /*   By: jeongwpa <jeongwpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 00:16:07 by jeongwpa          #+#    #+#             */
-/*   Updated: 2024/08/18 22:07:40 by jeongwpa         ###   ########.fr       */
+/*   Updated: 2024/08/18 22:15:18 by jeongwpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,20 +43,32 @@ void	init_mlx(t_rt *rt, t_img *img)
 
 void	init_viewport(t_img *img, t_camera *camera, t_viewport *viewport)
 {
+	t_vec3		look_from;
+	t_vec3		look_at;
+	t_vec3		view_up;
+	t_vec3		w;
+	t_vec3		u;
+	t_vec3		v;
 	float		viewport_height;
 	float		viewport_width;
 	t_vec3		viewport_u;
 	t_vec3		viewport_v;
 
-	viewport->focal_length = 1.0;
+	look_from = camera->view_point;
+	look_at = vec3_add(look_from, camera->normal);
+	view_up = (t_vec3){0, 1, 0};
+	viewport->focal_length = vec3_length(vec3_sub(look_from, look_at));
 	viewport_height = 2.0 * tan(degree_to_radian(camera->fov) / 2.0);
 	viewport_width = viewport_height * ((float)img->width / img->height);
-	viewport_u = (t_vec3){viewport_width, 0, 0};
-	viewport_v = (t_vec3){0, -viewport_height, 0};
+	w = vec3_unit(vec3_sub(look_from, look_at));
+	u = vec3_unit(vec3_cross(view_up, w));
+	v = vec3_cross(w, u);
+	viewport_u = vec3_mul(u, viewport_width);
+	viewport_v = vec3_mul(v, -viewport_height);
 	viewport->pixel_delta_u = vec3_div(viewport_u, img->width);
 	viewport->pixel_delta_v = vec3_div(viewport_v, img->height);
 	viewport->upper_left_corner = vec3_sub(camera->view_point, \
-		(t_vec3){0, 0, viewport->focal_length});
+		vec3_mul(w, viewport->focal_length));
 	viewport->upper_left_corner = vec3_sub(viewport->upper_left_corner, \
 		vec3_div(viewport_u, 2));
 	viewport->upper_left_corner = vec3_sub(viewport->upper_left_corner, \

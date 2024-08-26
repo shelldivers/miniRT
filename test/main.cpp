ab40bd52b1fcc7ada9d37cb819cda1242cab01a3
yhames <wjddn494@naver.com>
2024-08-26 10:03:57 +0900

    test: add test for parse texture

--- test/main.cpp
+++ test/main.cpp
@@ -15,6 +15,23 @@ void clear_get_next_line_buffer(int fd) {
 	}
 }
 
+extern "C" void	*mlx_xpm_file_to_image(void *xvar,char *file,int *width,int *height)
+{
+	(void)xvar;
+	(void)file;
+	*width = 42;
+	*height = 42;
+	return ((void *)42);
+}
+
+extern "C" void *mlx_get_data_addr(void *img_ptr, int *bits_per_pixel, int *size_line, int *endian) {
+	(void)img_ptr;
+	*bits_per_pixel = 42;
+	*size_line = 42;
+	*endian = 42;
+	return ((void *)42);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
